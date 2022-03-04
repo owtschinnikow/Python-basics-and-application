a69897ca0db43c513a82082f5f0df8fdb33dd420
OvchinnikovSV <OvchinnikovSV@proletarsky.ru>
2022-03-04 16:08:11 +0300

    add new file 1

---
+++ 1.4_10.1 пространства имён и области видимости.py
@@ -0,0 +1,21 @@
+"""
+Sample Input:
+9
+add global a
+create foo global
+add foo b
+get foo a
+get foo c
+create bar foo
+add bar a
+get bar a
+get bar b
+
+Sample Output:
+global
+None
+bar
+foo
+"""
+
+cmd, namesp, arg = input().split()

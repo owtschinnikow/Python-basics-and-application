f1e24b8a439c16c428b478109b00a193ece02af3
OvchinnikovSV <OvchinnikovSV@proletarsky.ru>
2022-03-14 11:13:51 +0300

    1.5_8.1 done

--- 1.5_8.1 введение в классы.py
+++ 1.5_8.1 введение в классы.py
@@ -8,29 +8,26 @@
 class MoneyBox:
     def __init__(self, capacity):
         """
-        # конструктор с аргументом – вместимость копилки
+        Конструктор с аргументом – вместимость копилки.
         """
         self.capacity = capacity
         self.number_of_coins = 0
 
     def can_add(self, v):
         """
-        # True, если можно добавить v монет, False иначе
+        True, если можно добавить v монет, False иначе.
         :param v:
         :return:
         """
-        if self.capacity - self.number_of_coins - self.v > 0:
-            print(True)
-        else:
-            print(False)
+        if self.capacity - self.number_of_coins - v >= 0:
+            return True
+        return False
 
     def add(self, v):
         """
-        # положить v монет в копилку
+        Положить v монет в копилку.
         :param v:
         :return:
         """
-        if self.can_add(self, v):
+        if self.can_add(v):
             self.number_of_coins += v
-        else:
-            print(False)

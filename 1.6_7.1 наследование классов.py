8861e05652024d974b38cc008bcb55e0f7632bca
OvchinnikovSV <OvchinnikovSV@proletarsky.ru>
2022-03-16 19:03:43 +0300

    1.6_7.1 update

--- 1.6_7.1 наследование классов.py
+++ 1.6_7.1 наследование классов.py
@@ -18,4 +18,16 @@ Yes
 No
 """
 
+def main():
+    number = int(input())
+    for i in range(number):
+        command, namespace, argument = input().split()
 
+        if command == 'create':
+            # print('create - ', n
+
+
+if __name__ == '__main__':
+    # import doctest
+    # doctest.testmod(verbose=True)
+    main()

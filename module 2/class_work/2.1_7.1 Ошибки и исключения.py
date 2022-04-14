fb1dcc9afc3450355f9b39eabd5d463e02dd29ad
OvchinnikovSV <OvchinnikovSV@proletarsky.ru>
2022-04-14 12:54:58 +0300

    Add 2.1_7.1 Ошибки и исключения.py

--- module 2/class_work/2.1_7.1 Ошибки и исключения.py
+++ module 2/class_work/2.1_7.1 Ошибки и исключения.py
@@ -16,7 +16,38 @@ FileNotFoundError
 """
 
 
+import collections
 
+def breadth_first_search(graph, root, peak):
+    if root not in graph:
+        return print('No')
+    visited, queue = set(), collections.deque([root])
+    visited.add(root)
+    while queue:
+        vertex = queue.popleft()
+        for neighbour in graph[vertex]:
+            if neighbour not in visited:
+                visited.add(neighbour)
+                queue.append(neighbour)
+    if peak in visited:
+        print('Yes')
+    else:
+        print('No')
+
+def main():
+    namespace_dict = {}
+    number = int(input())
+    for i in range(number):
+        data_input = input()
+        if ' : ' not in data_input:
+            namespace_dict[data_input] = []
+        else:
+            namespace_child, namespace_parent  = data_input.split(' : ')
+            namespace_dict[namespace_child] = [*namespace_parent.split()]
+    quantity = int(input())
+    for i in range(quantity):
+        namespace_parent, namespace_child = input().split()
+        breadth_first_search(namespace_dict, namespace_child, namespace_parent)
 
 if __name__ == '__main__':
     main()

b5610d08161615ce7d48f1b4e9fa6ba4929264f9
OvchinnikovSV <OvchinnikovSV@proletarsky.ru>
2022-05-11 21:05:01 +0300

    done

--- module 2/2.4_4.1/2.4_4.1 Работа с файловой системой и файлами.py
+++ module 2/2.4_4.1/2.4_4.1 Работа с файловой системой и файлами.py
@@ -1,4 +1,25 @@
-with open('dataset_24465_4.txt') as file, open('dataset_revers.txt', 'w') as revers:
-    for line in file:
-        revers.write(line)
-    reversed(revers)
+s = []
+with open('dataset_24465_4.txt') as file:
+    for n, line in enumerate(file):
+        s.append(line[:-1:])
+    #     print(n, line[:-1:])
+    # print(len(s))
+    # for n, i in enumerate(s):
+    #     print(n, i)
+
+s = s[::-1]
+for n, i in enumerate(s):
+    print(i)
+
+
+
+# with open('dataset_revers.txt', 'w') as revers:
+#     for m, i in enumerate(s):
+#         s_pop = s.pop() + '\n'
+#         print(m, s_pop, end='')
+#         revers.write(s_pop)
+
+
+lines = open("input.txt").readlines()
+with open("output.txt", "w") as out:
+    out.writelines(reversed(lines))

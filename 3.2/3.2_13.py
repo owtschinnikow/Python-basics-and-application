4a4aaebb909a9b828a7f12ab43614ee1920da3d7
OvchinnikovSV <OvchinnikovSV@proletarsky.ru>
2022-06-14 18:52:41 +0300

    3.2_13.py done

--- 3.2/3.2_13.py
+++ 3.2/3.2_13.py
@@ -17,7 +17,7 @@ import re
 
 for line in sys.stdin:
     line = line.rstrip()
-    pattern = r'([aA])+'
+    pattern = r'\b[a|A]+\b'
     result = re.search(pattern, line)
     if result != None:
         print(re.sub(pattern, 'argh', line, count=1, flags=re.IGNORECASE))

abcc8e61b76d40d8601fa2847ac2652df2da9429
OvchinnikovSV <OvchinnikovSV@proletarsky.ru>
2022-06-15 18:41:01 +0300

    3.2_15.py update

--- 3.2/3.2_15.py
+++ 3.2/3.2_15.py
@@ -4,10 +4,12 @@
 Sample Input:
 attraction
 buzzzz
+mewmewNnyaaa
 
 Sample Output:
 atraction
 buz
+mewmewNnya
 
 https://regex101.com/#python
 """
@@ -18,7 +20,7 @@ import re
 
 for line in sys.stdin:
     line = line.rstrip()
-    pattern = r'\b(\w)(\w)'
+    pattern = r'(\w)\1+'
     result = re.search(pattern, line)
-    if result != None:
-        print(re.sub(pattern, r"\2\1", line, count=0, flags=re.IGNORECASE))
+    print(re.sub(pattern, r"\1", line))
+
